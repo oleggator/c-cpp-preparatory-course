8c4c7b47a1aa18e3d9e67d41c0879921d7c40791
Oleg Utkin <oleg_utkin@me.com>
2016-10-27 17:58:57 +0300

    Add chrono library

--- homework5/RandomValues.h
+++ homework5/RandomValues.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <random>
 #include <ctime>
+#include <chrono>
 
 const char plateCharsCount = 12;
 const char plateChars[plateCharsCount] = {'A', 'B', 'E', 'K', 'M', 'H', 'P', 'O', 'C', 'T', 'Y', 'X'};
