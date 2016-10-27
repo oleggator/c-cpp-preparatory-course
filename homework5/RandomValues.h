b859012246ed9dfe227f16937f40927fc9704ceb
Oleg Utkin <oleg_utkin@me.com>
2016-10-27 18:02:08 +0300

    Add algorithm library

--- homework5/RandomValues.h
+++ homework5/RandomValues.h
@@ -7,6 +7,7 @@
 #include <random>
 #include <ctime>
 #include <chrono>
+#include <algorithm>
 
 const char plateCharsCount = 12;
 const char plateChars[plateCharsCount] = {'A', 'B', 'E', 'K', 'M', 'H', 'P', 'O', 'C', 'T', 'Y', 'X'};
